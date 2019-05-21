87fcf167b997bcd67837e5374e60e4d5ec70e824
Sam Deane <sam@elegantchaos.com>
2019-05-21 16:48:34 +0100

    Refactored common code.

--- Sources/Expressions/Expressions.swift
+++ Sources/Expressions/Expressions.swift
@@ -20,6 +20,25 @@ public protocol Constructable {
 }
 
 public extension NSRegularExpression {
+    
+    /**
+     Unpack a reg-exp match into a result structure.
+    */
+    
+    fileprivate func unpack<T>(_ match: NSTextCheckingResult, mappings: [PartialKeyPath<T> : Int], string: String, into: inout T) {
+        for mapping in mappings {
+            if let range = Range(match.range(at: mapping.value), in: string) {
+                if let path = mapping.key as? WritableKeyPath<T,String> {
+                    let captured = String(string[range])
+                    into[keyPath: path] = captured
+                } else if let path = mapping.key as? WritableKeyPath<T,Int> {
+                    let captured = (String(string[range]) as NSString).integerValue
+                    into[keyPath: path] = captured
+                }
+            }
+        }
+    }
+    
     /**
      Finds the first match of the expression in the given string.
      Uses the mappings to fill in a result instance, which is returned.
@@ -35,17 +54,7 @@ public extension NSRegularExpression {
         let range = NSRange(location: 0, length: string.count)
         if let match = firstMatch(in: string, options: [], range: range) {
             var result = T()
-            for mapping in mappings {
-                if let range = Range(match.range(at: mapping.value), in: string) {
-                    if let path = mapping.key as? WritableKeyPath<T,String> {
-                        let captured = String(string[range])
-                        result[keyPath: path] = captured
-                    } else if let path = mapping.key as? WritableKeyPath<T,Int> {
-                        let captured = (String(string[range]) as NSString).integerValue
-                        result[keyPath: path] = captured
-                    }
-                }
-            }
+            unpack(match, mappings: mappings, string: string, into: &result)
             return result
         }
         return nil
@@ -66,15 +75,7 @@ public extension NSRegularExpression {
     func firstMatch<T>(in string: String, capturing mappings: [PartialKeyPath<T>:Int], into capture: inout T) -> Bool {
         let range = NSRange(location: 0, length: string.count)
         if let match = firstMatch(in: string, options: [], range: range) {
-            for mapping in mappings {
-                if let range = Range(match.range(at: mapping.value), in: string) {
-                    if let path = mapping.key as? ReferenceWritableKeyPath<T,String> {
-                        capture[keyPath: path] = String(string[range])
-                    } else if let path = mapping.key as? ReferenceWritableKeyPath<T,Int> {
-                        capture[keyPath: path] = (String(string[range]) as NSString).integerValue
-                    }
-                }
-            }
+            unpack(match, mappings: mappings, string: string, into: &capture)
             return true
         }
         
