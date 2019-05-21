4200b392e0ecedd70e3d284c9ebc0950b7374212
Sam Deane <sam@elegantchaos.com>
2019-05-21 15:45:21 +0100

    Initial code from Bookish

--- Sources/Expressions/Expressions.swift
+++ Sources/Expressions/Expressions.swift
@@ -1,3 +1,51 @@
-struct Expressions {
-    var text = "Hello, World!"
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 05/12/2018.
+//  All code (c) 2018 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+import Foundation
+
+public protocol RegularExpressionResult {
+    init()
+}
+
+public extension NSRegularExpression {
+    func firstMatch<T: RegularExpressionResult>(of string: String, capturing mappings: [PartialKeyPath<T>:Int]) -> T? {
+        let range = NSRange(location: 0, length: string.count)
+        if let match = firstMatch(in: string, options: [], range: range) {
+            var result = T()
+            for mapping in mappings {
+                if let range = Range(match.range(at: mapping.value), in: string) {
+                    if let path = mapping.key as? WritableKeyPath<T,String> {
+                        let captured = String(string[range])
+                        result[keyPath: path] = captured
+                    } else if let path = mapping.key as? WritableKeyPath<T,Int> {
+                        let captured = (String(string[range]) as NSString).integerValue
+                        result[keyPath: path] = captured
+                    }
+                }
+            }
+            return result
+        }
+        return nil
+    }
+    
+    func firstMatch<T>(of string: String, capturing mappings: [PartialKeyPath<T>:Int], into capture: inout T) -> Bool {
+        let range = NSRange(location: 0, length: string.count)
+        if let match = firstMatch(in: string, options: [], range: range) {
+            for mapping in mappings {
+                if let range = Range(match.range(at: mapping.value), in: string) {
+                    if let path = mapping.key as? ReferenceWritableKeyPath<T,String> {
+                        capture[keyPath: path] = String(string[range])
+                    } else if let path = mapping.key as? ReferenceWritableKeyPath<T,Int> {
+                        capture[keyPath: path] = (String(string[range]) as NSString).integerValue
+                    }
+                }
+            }
+            return true
+        }
+        
+        return false
+    }
+    
 }
--- Tests/ExpressionsTests/ExpressionsTests.swift
+++ Tests/ExpressionsTests/ExpressionsTests.swift
@@ -1,15 +1,54 @@
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+//  Created by Sam Deane on 07/12/2018.
+//  All code (c) 2018 - present day, Elegant Chaos Limited.
+// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
 import XCTest
 @testable import Expressions
 
-final class ExpressionsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Expressions().text, "Hello, World!")
+class ExpressionTests: XCTestCase {
+    
+    func testReturnedResults() {
+        struct Result: RegularExpressionResult {
+            var first = ""
+            var last = ""
+            var number = 0
+        }
+        
+        let pattern = try! NSRegularExpression(pattern: "(\\w+) (.*) (\\w+)", options: [])
+        
+        if let match: Result = pattern.firstMatch(of: "Sam 123 Deane", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2]) {
+            XCTAssertEqual(match.first, "Sam")
+            XCTAssertEqual(match.last, "Deane")
+            XCTAssertEqual(match.number, 123)
+        }
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
+    func testPassedInResults() {
+        class Result {
+            var first = ""
+            var last = ""
+            var number = 0
+        }
+        
+        let pattern = try! NSRegularExpression(pattern: "(\\w+) (.*) (\\w+)", options: [])
+        var result = Result()
+        if pattern.firstMatch(of: "Sam 123 Deane", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2], into: &result) {
+            XCTAssertEqual(result.first, "Sam")
+            XCTAssertEqual(result.last, "Deane")
+            XCTAssertEqual(result.number, 123)
+        }
+    }
+    
+    func testPassedInResultsNoMatch() {
+        class Result {
+            var first = ""
+            var last = ""
+            var number = 0
+        }
+        
+        let pattern = try! NSRegularExpression(pattern: "(\\w+) (.*) (\\w+)", options: [])
+        var result = Result()
+        XCTAssertFalse(pattern.firstMatch(of: "Wrong", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2], into: &result))
+    }
 }
