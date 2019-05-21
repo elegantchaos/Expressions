bef989a852c93115e4ce4a9c0c8b8d461932574d
Sam Deane <sam@elegantchaos.com>
2019-05-21 16:36:36 +0100

    Improved comments.

--- Sources/Expressions/Expressions.swift
+++ Sources/Expressions/Expressions.swift
@@ -5,12 +5,33 @@
 
 import Foundation
 
-public protocol RegularExpressionResult {
+/**
+ A protocol that result types need to conform to in order for them to
+ be useable with methods that create and return instances of them.
+ 
+ This is necessary because there is no standard way to express that a type
+ must be constructable with an initialiser that takes no parameters
+ (ideally this would be a standard swift protocol that types
+ automatically conformed to if they matched the criteria)
+ */
+
+public protocol Constructable {
     init()
 }
 
 public extension NSRegularExpression {
-    func firstMatch<T: RegularExpressionResult>(of string: String, capturing mappings: [PartialKeyPath<T>:Int]) -> T? {
+    /**
+     Finds the first match of the expression in the given string.
+     Uses the mappings to fill in a result instance, which is returned.
+ 
+     Because this method constructs the result instance, the instance type needs to conform to
+     `Constructable`.
+     
+     @param in The string to search.
+     @param mappings A dictionary mapping key paths in the result type to capture groups in the expression.
+    */
+    
+    func firstMatch<T: Constructable>(in string: String, capturing mappings: [PartialKeyPath<T>:Int]) -> T? {
         let range = NSRange(location: 0, length: string.count)
         if let match = firstMatch(in: string, options: [], range: range) {
             var result = T()
@@ -29,8 +50,19 @@ public extension NSRegularExpression {
         }
         return nil
     }
-    
-    func firstMatch<T>(of string: String, capturing mappings: [PartialKeyPath<T>:Int], into capture: inout T) -> Bool {
+
+    /**
+     Finds the first match of the expression in the given string.
+     Uses the mappings to fill in the passed-in result instance.
+
+     Because this variation fills in a passed-in result structure, it doesn't need to construct it. This
+     avoids the structure having to conform to `Constructable`.
+     
+     @param in The string to search.
+     @param mappings A dictionary mapping key paths in the result type to capture groups in the expression.
+     */
+
+    func firstMatch<T>(in string: String, capturing mappings: [PartialKeyPath<T>:Int], into capture: inout T) -> Bool {
         let range = NSRange(location: 0, length: string.count)
         if let match = firstMatch(in: string, options: [], range: range) {
             for mapping in mappings {
@@ -48,6 +80,23 @@ public extension NSRegularExpression {
         return false
     }
     
+    /**
+     Finds the first match of the expression in the given string.
+     Uses Swift reflection to fill in the passed-in result instance, by looking for named capture groups
+     that match the names of properties in the instance.
+     
+     Because this method constructs the result instance, the instance type needs to conform to
+     `Constructable`.
+     
+     Because it relies on named capture groups, the regular expression needs to use them.
+
+     Because Swift reflection currently only supports read-only access, we have to use Objective-C
+     key-value support to write into the result instance. This means that any captured properties have
+     to be annotated with @objc, as does the structure itself.
+     
+     @param in The string to search.
+     */
+
     @available(macOS 10.13, iOS 10.0, *) func firstMatch<T>(in string: String) -> T? where T: NSObject {
         let over = NSRange(location: 0, length: string.count)
         if let match = firstMatch(in: string, options: [], range: over) {
--- Tests/ExpressionsTests/ExpressionsTests.swift
+++ Tests/ExpressionsTests/ExpressionsTests.swift
@@ -9,7 +9,7 @@ import XCTest
 class ExpressionTests: XCTestCase {
     
     func testReturnedResults() {
-        struct Result: RegularExpressionResult {
+        struct Result: ExpressionResult {
             var first = ""
             var last = ""
             var number = 0
@@ -17,7 +17,7 @@ class ExpressionTests: XCTestCase {
         
         let pattern = try! NSRegularExpression(pattern: "(\\w+) (.*) (\\w+)", options: [])
         
-        if let match: Result = pattern.firstMatch(of: "Sam 123 Deane", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2]) {
+        if let match: Result = pattern.firstMatch(in: "Sam 123 Deane", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2]) {
             XCTAssertEqual(match.first, "Sam")
             XCTAssertEqual(match.last, "Deane")
             XCTAssertEqual(match.number, 123)
@@ -33,7 +33,7 @@ class ExpressionTests: XCTestCase {
         
         let pattern = try! NSRegularExpression(pattern: "(\\w+) (.*) (\\w+)", options: [])
         var result = Result()
-        if pattern.firstMatch(of: "Sam 123 Deane", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2], into: &result) {
+        if pattern.firstMatch(in: "Sam 123 Deane", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2], into: &result) {
             XCTAssertEqual(result.first, "Sam")
             XCTAssertEqual(result.last, "Deane")
             XCTAssertEqual(result.number, 123)
@@ -49,7 +49,7 @@ class ExpressionTests: XCTestCase {
         
         let pattern = try! NSRegularExpression(pattern: "(\\w+) (.*) (\\w+)", options: [])
         var result = Result()
-        XCTAssertFalse(pattern.firstMatch(of: "Wrong", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2], into: &result))
+        XCTAssertFalse(pattern.firstMatch(in: "Wrong", capturing: [\Result.first: 1, \Result.last: 3, \Result.number: 2], into: &result))
     }
     
     @available(macOS 10.13, iOS 10.0, *) func testReflection() {
